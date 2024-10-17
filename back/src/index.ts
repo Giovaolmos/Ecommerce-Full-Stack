174a5ed64519d94929e33d6c510fbf7b8a45afa0
Giovaolmos <127804984+Giovaolmos@users.noreply.github.com>
2024-10-17 01:17:08 -0300

    creacion de cliente

--- back/src/index.ts
+++ back/src/index.ts
@@ -16,7 +16,7 @@ const initialize = async () => {
   await preLoadCategories();
   await preLoadProducts();
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port http://localhost:${PORT}`);
   });
 };
 
